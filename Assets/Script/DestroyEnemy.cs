342299052189b90d2d55bedfc9d92a8f9952e9ef
Syadu Budi Kusuma <cybersyadu5@gmail.com>
2024-06-19 11:21:47 +0700

    SpawnEnemy

--- Assets/Script/DestroyEnemy.cs
+++ Assets/Script/DestroyEnemy.cs
@@ -2,24 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bullet : MonoBehaviour
+public class DestroyEnemy : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(DestroyAfter()); 
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         
-    }
-
-    IEnumerator DestroyAfter()
-    {
-        yield return new WaitForSeconds(3f);
-        Destroy(gameObject);
     }
 
     private void OnCollisionEnter(Collision other)
